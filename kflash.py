7d91f1db2a17d4b84bf1917c8675ca19c9308e0b
Huang Rui <vowstar@gmail.com>
2019-04-29 17:32:27 +0800

    Remove absolute_import, unicode_literals import
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- kflash.py
+++ kflash.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from __future__ import (division, print_function, absolute_import, unicode_literals)
+from __future__ import (division, print_function)
 
 import sys
 import time
